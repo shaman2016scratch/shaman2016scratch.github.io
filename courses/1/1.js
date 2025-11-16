6d7308da7a571ed4814980bfbbfc4b43c81380b7
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-16 22:44:26 +0300

    Update 1.js

--- courses/1/1.js
+++ courses/1/1.js
@@ -1,1 +1,12 @@
-
+root = {}
+root.doc = document.getElementById('root')
+strans = {
+  1: `
+    <h1>Введение в RussiaScript, урок 1</h1>
+    <p>Это Оффициальный курс по введению в RussiaScript.</p>
+  `
+}
+root.stran = 1
+function root() {
+  root.stran
+}
