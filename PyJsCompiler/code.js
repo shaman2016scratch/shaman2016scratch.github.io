e65fcaaee762c91a0bf5c041d7b23f044567a0f4
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-14 09:42:03 +0300

    Update code.js

--- PyJsCompiler/code.js
+++ PyJsCompiler/code.js
@@ -1,8 +1,8 @@
 let PyJsCompiler = {
   "pyOut": [],
-  "pyVar": {} 
+  "pyVar": {},
   "compile": {
     "py_js": function(c) { let code = c.split("\n"); let comp = ""; for(let i = 0; i < code.length; i++){ let i2 = code[i].split(' '); if(i2[0] === 'print'){ comp = `${comp}PyJsCompiler.pyOut.push${i2[0]}; ` }else if(i2[0] === 'import'){}else if(i2[1] === '='){ comp = `${comp}PyJsCompiler.pyVar${i2[0]} = ${i2[2]}; ` } } }
   },
-  "Terminal": function(tc) { let Term = tc.split("\n") }
+  "Terminal": function(tc) { let term = tc.split("\n"); for(let i = 0; i < term.length; i++){ console.error('[python] not terminal support') } }
 }
