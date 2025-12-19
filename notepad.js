b709d29586000ed1801cf4c79fb009324659843f
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-19 21:32:05 +0300

    Update notepad.js

--- notepad.js
+++ notepad.js
@@ -1,4 +1,5 @@
 const txt = {
   "save": function() { localStorage.set(`notepad_${document.getElementById('name').input}`, document.getElementById('txt').input) },
-  "load": function() { document.getElementById('txt').input = localStorage.get(`notepad_${document.getElementById('name').input}`) }
+  "load": function() { document.getElementById('txt').input = localStorage.get(`notepad_${document.getElementById('name').input}`) },
+  "xy": function() { document.getElementById('txt').rows = document.getElementById('xy').split('x')[0]; document.getElementById('txt').cols = document.getElementById('xy').split('x')[1] }
 }
