9a0c250f5f6b3e5d1642b61824e32eab0f3f45e2
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-31 00:00:28 +0300

    Update top.js

--- Explore2/top.js
+++ Explore2/top.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./top.css";
-
 const rootEl = document.getElementById("topboard");
 const root = createRoot(rootEl);
-
-root.render();
+let projects_req = fetch("https://shaman2016-trampline.vercel.app/scratch/studios/36416387/projects")
+let projects = projects_req.json()
+for(let i = 0; i < projects.length; i++){}
+root.render(app);
