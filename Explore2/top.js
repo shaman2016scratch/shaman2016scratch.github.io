5bf753ebca3e33b9e56c5bf283048983d8b9c11d
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-30 23:50:22 +0300

    Create top.js

---
+++ Explore2/top.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./top.css";
+
+const rootEl = document.getElementById("topboard");
+const root = createRoot(rootEl);
+
+root.render();
