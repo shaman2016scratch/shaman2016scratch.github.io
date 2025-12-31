2a52719aada6805f86cd11609361c27b4176f3a6
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-31 14:58:23 +0300

    Update index.js

--- PengVillage/index.js
+++ PengVillage/index.js
@@ -2,7 +2,11 @@ try {
   let game = document.getElementById('game')
   let menu = document.getElementById('menu')
   let name = document.getElementById('username')
-  class Game {}
+  class Game {
+    game.innerHTML("<text x="0" y="40" font-size="36" fill="black">What is your name? This is necessary for this game.</text>")
+    setTimeout(() => {}, 5000);
+    game.innerHTML(`<text x="0" y="40" font-size="36" fill="black">Hello, ${name}, welcome to Part 1 of the Peng Village game!</text>`)
+  }
   function start() { new Game() }
   document.addEventListener('DOMContentLoaded', start);
 } catch (err) {
