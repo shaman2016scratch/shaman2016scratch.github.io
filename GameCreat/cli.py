395c1ec59018df4ff4288490bfeebeb22cfc7fad
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-01 02:29:28 +0300

    Update cli.py

--- GameCreat/cli.py
+++ GameCreat/cli.py
@@ -1,3 +1,9 @@
 print("GameCreat CLI")
 while True:
   cmd = input("user@virtual-cli: >> ")
+  if cmd == "client":
+  	print("CLI: GameCreat CLI 0.0.1 (January 1, 2025 02:30 UTC+3)")
+  	print("GameCreat version: 1.0")
+  else:
+  	print("/home/runner/work/shaman2016scratch.github.io/shaman2016scratch.github.io/GameCreat/cli.py")
+  	print("Command not found")
