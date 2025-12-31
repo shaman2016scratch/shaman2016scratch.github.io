d42d669ce01ce7dd49aa16a250d804144a40a645
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-31 13:17:23 +0300

    Update index.js

--- PengVillage/index.js
+++ PengVillage/index.js
@@ -1,1 +1,9 @@
-
+try {
+  let game = document.getElementById('game')
+  let menu = document.getElementById('menu')
+  let name = document.getElementById('username')
+  class Game {}
+  function start() {}
+} catch (err) {
+  alert(`Error: ${err.message}`); console.error('')
+}
