87a43a694968a1ebe11d215d60e4b39964e1c829
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-31 15:16:43 +0300

    Update index.js

--- PengVillage/index.js
+++ PengVillage/index.js
@@ -1,14 +1,10 @@
-try {
-  let game = document.getElementById('game')
-  let menu = document.getElementById('menu')
-  let name = document.getElementById('username')
-  class Game {
-    game.innerHTML('<svg width="800" height="400" xmlns="http://www.w3.org/2000/svg"><text x="0" y="40" font-size="36" fill="black">What is your name? This is necessary for this game.</text></svg>')
-    setTimeout(() => {}, 5000);
-    game.innerHTML(`<svg width="800" height="400" xmlns="http://www.w3.org/2000/svg"><text x="0" y="40" font-size="36" fill="black">Hello, ${name}, welcome to Part 1 of the Peng Village game!</text></svg>`)
-  }
-  function start() { new Game() }
-} catch (err) {
-  alert(`Error: ${err.message}`); console.error(`Error: ${err.message}`)
+let game = document.getElementById('game')
+let menu = document.getElementById('menu')
+let name = document.getElementById('username')
+class Game {
+  game.innerHTML('<svg width="800" height="400" xmlns="http://www.w3.org/2000/svg"><text x="0" y="40" font-size="36" fill="black">What is your name? This is necessary for this game.</text></svg>')
+  setTimeout(() => {}, 5000);
+  game.innerHTML(`<svg width="800" height="400" xmlns="http://www.w3.org/2000/svg"><text x="0" y="40" font-size="36" fill="black">Hello, ${name}, welcome to Part 1 of the Peng Village game!</text></svg>`)
 }
+function start() { new Game() }
 document.addEventListener('DOMContentLoaded', start);
