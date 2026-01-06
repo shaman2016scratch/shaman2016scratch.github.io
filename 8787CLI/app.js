bfd34f0f610b3c1f69ff23c9728f2e7ec682d1ea
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-06 11:37:40 +0300

    Update app.js

--- 8787CLI/app.js
+++ 8787CLI/app.js
@@ -1,1 +1,1 @@
- const app8787 = new Function(fetch('https://shaman2016scratch.github.io/app8787/func.js'))
+const app8787 = new Function(fetch('https://shaman2016scratch.github.io/app8787/func.js'))
