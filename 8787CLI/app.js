bb58a93e730898dd7e1dae66f2f3a5a8b86256f6
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-06 11:55:16 +0300

    Оставим app8787 на будущее

--- 8787CLI/app.js
+++ 8787CLI/app.js
@@ -1,5 +1,1 @@
-let app = fetch("https://shaman2016scratchmgithub.io/8787CLI/app.app87"); let app8787d = {}; const app8787f = new Function(fetch('https://shaman2016scratch.github.io/app8787/func.js')); const app8787 = app8787f();
-app8787.installer('app8787'); app8787.app.new('id1', document.body);
-app8787.app.set('id1', `
-  <h1>8787CLI</h1> 
-`)
+
