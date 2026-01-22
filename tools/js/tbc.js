d443053ca4b24a2a3b2c87306592c8aab5f67179
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-22 19:45:44 +0300

    Update tbc.js

--- tools/js/tbc.js
+++ tools/js/tbc.js
@@ -1,5 +1,7 @@
 try {
   const screen = document.getElementById("content")
+  const token = document.getElementById("bot")
+  let chats = []
   screen.innerHTML = `
     <h1>Chats</h1>
     <div id="chats">loading...</div>
@@ -9,6 +11,9 @@ try {
   }
   function load() {
     // Не готово
+  }
+  function start() {
+    let messages = fetch(`https://api.telegram.org/bot${token}/getUpdates/`)
   }
 } catch (err) {
   alert(`Error: ${err.message}`)
