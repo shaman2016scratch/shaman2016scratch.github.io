73a33dfab24b895761c171aeeefd2383891ad5cd
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-22 18:54:21 +0300

    Update tbc.js

--- tools/js/tbc.js
+++ tools/js/tbc.js
@@ -1,1 +1,10 @@
-const screen = document.getElementById
+try {
+  const screen = document.getElementById("content")
+  screen.innerHTML = `
+    <h1>Chats</h1>
+    <div id="chats">loading...</div>
+  `
+} catch (err) {
+  alert(`Error: ${err.message}`)
+  console.error(err.message)
+}
