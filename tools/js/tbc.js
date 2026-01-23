c740d2675f641dc60276715b058ccba2731e6de9
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-23 12:56:13 +0300

    Update tbc.js

--- tools/js/tbc.js
+++ tools/js/tbc.js
@@ -1,6 +1,6 @@
 try {
   const screen = document.getElementById("content")
-  const token = document.getElementById("bot").input
+  const token = document.getElementById("bot").value
   let chats = []
   let chatInfo = {}
   let openChat = {}
@@ -14,13 +14,11 @@ try {
   function load() {
     // Не готово
   }
-  function start() {
+  async function start() {
     let messages = await (await fetch(`https://api.telegram.org/bot${token}/getUpdates/`)).json().result
     async function getChats() {
       for(let i = 0; i < messages.length; i++) {
-        if (chats[messages[i].chat.id]) {
-          // Пока игнорим
-        } else {
+        if (!chats.includes(messages[i].chat.id)) {
           chats.push(messages[i].chat.id)
         }
         chatInfo[messages[i].chat.id] = {
@@ -30,21 +28,22 @@ try {
         }
       }
     }
-    getChats()
+    await getChats()
     async function addChats() {
       screen.innerHTML = `
         <div id="chats"><h1>Chats</h1></div>
       `
       let chatsList = document.getElementById("chats")
       for(let i = 0; i < chats.length; i++) {
-        chatsList.innerHTML = `${chatList.innerHTML}
-        <h1 onclick="openChat.${i}()"><img src="${chatInfo[chats[i]].icon}">${chatInfo[chats[i]].name}</h1>`
+        chatsList.innerHTML += `
+          <h1 onclick="openChat[${i}]()"><img src="${chatInfo[chats[i]].icon}">${chatInfo[chats[i]].name}</h1>
+        `
         openChat[i] = new Function(`
-          chat(chats[i])
+          chat(${chats[i]})
         `)
       }
     }
-    addChats()
+    await addChats()
     async function chat(id) {
       screen.innerHTML = `
         <div id="messages" class="messages"><h1><img src="${chatInfo[id].icon}">${chatInfo[id].name}</h1></div>
@@ -52,23 +51,24 @@ try {
       let messList = document.getElementById("messages")
       async function getMess() {
         for(let i = 0; i < messages.length; i++) {
-          if(!messages[i].message.from.id === 777000) {
+          if(messages[i].message.from.id !== 777000) {
             if(messages[i].message.from.is_bot) {
-              messList.innerHTML `${messList.innerHTML}
-                <div class="message" id="id${messages[i].update_id}"><p><img src="https://placehold.co/100x100"><p><h4>${messages[i].message.from.first_name} [bot] <code>${messages[i].message.from.id}</code></h4><br>${messages[i].message.text}</p></p></div>
+              messList.innerHTML += `
+                <div class="message" id="id${messages[i].update_id}"><div><img src="https://placehold.co/100x100"><div><h4>${messages[i].message.from.first_name} [bot] <code>${messages[i].message.from.id}</code></h4><br>${messages[i].message.text}</div></div></div>
               `
             } else {
-              messList.innerHTML `${messList.innerHTML}
-                <div class="message" id="id${messages[i].update_id}"><p><img src="https://placehold.co/100x100"><p><h4>${messages[i].message.from.first_name} [user] <code>${messages[i].message.from.id}</code></h4><br>${messages[i].message.text}</p></p></div>
+              messList.innerHTML += `
+                <div class="message" id="id${messages[i].update_id}"><div><img src="https://placehold.co/100x100"><div><h4>${messages[i].message.from.first_name} [user] <code>${messages[i].message.from.id}</code></h4><br>${messages[i].message.text}</div></div></div>
               `
             }
           } else {
-            messList.innerHTML `${messList.innerHTML}
-              <div class="message" id="id${messages[i].update_id}"><p><img src="https://placehold.co/100x100"><p><h4>${messages[i].message.sender_chat.title} [${messages[i].message.sender_chat.type}] <code>${messages[i].message.sender_chat.id}</code></h4><br>${messages[i].message.text}</p></p></div>
+            messList.innerHTML += `
+              <div class="message" id="id${messages[i].update_id}"><div><img src="https://placehold.co/100x100"><div><h4>${messages[i].message.sender_chat.title} [${messages[i].message.sender_chat.type}] <code>${messages[i].message.sender_chat.id}</code></h4><br>${messages[i].message.text}</div></div></div>
             `
           }
         }
       }
+      await getMess()
     }
   }
 } catch (err) {
