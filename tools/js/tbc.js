ecf3bdcf3a5113bbe309a27304c17a82411d77c7
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-23 13:50:01 +0300

    Update tbc.js

--- tools/js/tbc.js
+++ tools/js/tbc.js
@@ -8,13 +8,18 @@ try {
     <h1>Chats</h1>
     <div id="chats">loading...</div>
   `
-  function save() {
-    // Не готово
-  }
-  function load() {
-    // Не готово
-  }
-  async function start() {
+} catch (err) {
+  alert(`Error: ${err.message}`)
+  console.error(err.message)
+}
+function save() {
+  // Не готово
+}
+function load() {
+  // Не готово
+}
+async function start() {
+  try {
     let messages = await (await fetch(`https://api.telegram.org/bot${token}/getUpdates/`)).json().result
     async function getChats() {
       for(let i = 0; i < messages.length; i++) {
@@ -47,34 +52,39 @@ try {
       }
     }
     await addChats()
-    async function chat(id) {
-      screen.innerHTML = `
-        <div id="messages" class="messages"><h1><img src="${chatInfo[id].icon}">${chatInfo[id].name}</h1></div>
-      `
-      let messList = document.getElementById("messages")
-      async function getMess() {
-        for(let i = 0; i < messages.length; i++) {
-          if(messages[i].message.from.id !== 777000) {
-            if(messages[i].message.from.is_bot) {
-              messList.innerHTML += `
-                <div class="message" id="id${messages[i].update_id}"><div><img src="https://placehold.co/100x100"><div><h4>${messages[i].message.from.first_name} [bot] <code>${messages[i].message.from.id}</code></h4><br>${messages[i].message.text}</div></div></div>
-              `
-            } else {
-              messList.innerHTML += `
-                <div class="message" id="id${messages[i].update_id}"><div><img src="https://placehold.co/100x100"><div><h4>${messages[i].message.from.first_name} [user] <code>${messages[i].message.from.id}</code></h4><br>${messages[i].message.text}</div></div></div>
-              `
-            }
+  } catch (err) {
+    alert(`Error: ${err.message}`)
+    console.error(err.message)
+  }
+}
+async function chat(id) {
+  try {
+    screen.innerHTML = `
+      <div id="messages" class="messages"><h1><img src="${chatInfo[id].icon}">${chatInfo[id].name}</h1></div>
+    `
+    let messList = document.getElementById("messages")
+    async function getMess() {
+      for(let i = 0; i < messages.length; i++) {
+        if(messages[i].message.from.id !== 777000) {
+          if(messages[i].message.from.is_bot) {
+            messList.innerHTML += `
+              <div class="message" id="id${messages[i].update_id}"><div><img src="https://placehold.co/100x100"><div><h4>${messages[i].message.from.first_name} [bot] <code>${messages[i].message.from.id}</code></h4><br>${messages[i].message.text}</div></div></div>
+            `
           } else {
             messList.innerHTML += `
-              <div class="message" id="id${messages[i].update_id}"><div><img src="https://placehold.co/100x100"><div><h4>${messages[i].message.sender_chat.title} [${messages[i].message.sender_chat.type}] <code>${messages[i].message.sender_chat.id}</code></h4><br>${messages[i].message.text}</div></div></div>
+              <div class="message" id="id${messages[i].update_id}"><div><img src="https://placehold.co/100x100"><div><h4>${messages[i].message.from.first_name} [user] <code>${messages[i].message.from.id}</code></h4><br>${messages[i].message.text}</div></div></div>
             `
           }
+        } else {
+          messList.innerHTML += `
+            <div class="message" id="id${messages[i].update_id}"><div><img src="https://placehold.co/100x100"><div><h4>${messages[i].message.sender_chat.title} [${messages[i].message.sender_chat.type}] <code>${messages[i].message.sender_chat.id}</code></h4><br>${messages[i].message.text}</div></div></div>
+          `
         }
       }
-      await getMess()
     }
+    await getMess()
+  } catch (err) {
+    alert(`Error: ${err.message}`)
+    console.error(err.message)
   }
-} catch (err) {
-  alert(`Error: ${err.message}`)
-  console.error(err.message)
 }
