797f2dabd1f5333d5233393a595f03fab25de254
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-24 15:16:03 +0300

    Update tbc.js

--- tools/js/tbc.js
+++ tools/js/tbc.js
@@ -158,5 +158,5 @@ async function Chat(id) {
 }
 async function sendMessage(chat) {
   fetch(`https://api.telegram.org/bot${token.value}/sendMessage?chat_id=${chat}&text=${document.getElementById("messageText").value}`)
-  Chat(chat)
+  start(); Chat(chat)
 }
