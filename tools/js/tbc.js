2a454091b4029fffa96f5a511e0f59e98fa53ced
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-24 10:55:22 +0300

    Update tbc.js

--- tools/js/tbc.js
+++ tools/js/tbc.js
@@ -28,8 +28,9 @@ async function start() {
           }
           chatInfo[messages[i][messHead].chat.id] = {
             "username": messages[i][messHead].chat.username || "",
-            "name": messages[i][messHead].chat.title,
-            "icon": "https://placehold.co/25x25"
+            "name": messages[i][messHead].chat.title || "noname",
+            "icon": "https://placehold.co/25x25",
+            "type": "private"
           }
           let icoon = await (await fetch(`https://api.telegram.org/bot${token.value}/getChat?chat_id=${messages[i][messHead].chat.id}`)).json()
           if(icoon.result.photo) {
@@ -37,6 +38,7 @@ async function start() {
             icoon = await (await fetch(`https://api.telegram.org/bot${token.value}/getFile?file_id=${icoon}`)).json()
             icoon = icoon.result.file_path
             chatInfo[messages[i][messHead].chat.id].icon = `https://api.telegram.org/file/bot${token.value}/${icoon}`
+            chatInfo[messages[i][messHead].chat.id].type = messages[i][messHead].chat.type
           }
         }
       }
@@ -49,7 +51,7 @@ async function start() {
       let chatsList = document.getElementById("chats")
       for(let i = 0; i < chats.length; i++) {
         chatsList.innerHTML += `
-          <h1 onclick="openChat[${i}]()"><img src="${chatInfo[chats[i]].icon}" width="25" height="25">${chatInfo[chats[i]].name}</h1>
+          <h1 onclick="openChat[${i}]()"><img src="${chatInfo[chats[i]].icon}" width="25" height="25">${chatInfo[chats[i]].name} [${chatInfo[chats[i]].type}]</h1>
         `
         openChat[i] = new Function(`
           chat(${chats[i]})
@@ -72,7 +74,7 @@ async function chat(id) {
       for(let i = 0; i < messages.length; i++) {
         if (messages[i].message) { messHead = "message" } else if (messages[i].channel_post) { messHead = "channel_post" } else if (messages[i].edited_message) { messHead = "edited_message" } else { messHead = "notSupport" }
         if(messHead !== "notSupport" && messages[i][messHead].chat.id === id) {
-          if(messages[i][messHead].from.id !== 777000) {
+          if(!messages[i][messHead].sender_chat) {
             if(messages[i][messHead].from.is_bot) {
               messList.innerHTML += `
                 <div class="message" id="id${messages[i].update_id}"><div><img src="https://placehold.co/25x25"><div><h4>${messages[i][messHead].from.first_name} [bot] <code>${messages[i][messHead].from.id}</code></h4><br>${messages[i][messHead].text}</div></div></div>
