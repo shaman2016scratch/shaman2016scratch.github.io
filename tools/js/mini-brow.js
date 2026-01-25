915b0ec058344a111677b6d5028801d3b961f624
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-26 01:01:43 +0300

    Update mini-brow.js

--- tools/js/mini-brow.js
+++ tools/js/mini-brow.js
@@ -4,7 +4,10 @@ async function save() {}
 async function start() {
   screen.innerHTML = `
     <h1>Mini Browser (IFRAME)</h1>
-    <button>add</button>
+    <button onclick="vkl.add()">add</button>
     <div id="vkladki"></div>
   `
+let vkl = {
+  "add": async function() {},
+  "del": async function() {}
 }
