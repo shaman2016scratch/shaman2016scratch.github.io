02025d10738181e5eb5f712fcaa1a6d511bbe2cf
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-26 14:59:39 +0300

    Update search.js

--- tools/js/search.js
+++ tools/js/search.js
@@ -1,5 +1,18 @@
 let screen = document.getElementById("content")
-const pages = []
+const site = {
+  "https://yandex.ru": {},
+  "https://google.com": {},
+  "https://scratch.mit.edu": {},
+  "https://github.com": {},
+  "https://shaman2016scratch.github.io": {}
+}
+const page = {
+  "https://yandex.ru/": {},
+  "https://google.com/": {},
+  "https://scratch.mit.edu/": {},
+  "https://github.com/": {},
+  "https://shaman2016scratch.github.io/": {}
+}
 async function save() {}
 async function load() {}
 async function start() {}
