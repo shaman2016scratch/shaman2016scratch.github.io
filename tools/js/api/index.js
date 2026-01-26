9c383c6aeef664cc1dd89052525a6563dff0a88e
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-26 16:00:31 +0300

    Update index.js

--- tools/js/api/index.js
+++ tools/js/api/index.js
@@ -2,17 +2,6 @@ let out = document.body
 const url = new URL(window.location)
 const input = url.searchParams
 if(!input.get("page") || input.get("page") === "main") {
-  ouut = {
-    "ok": true,
-    "result": {
-      "pagesCount": 3,
-      "pages": [
-        "tbc.html",
-        "mini-brow.html",
-        "search.html"
-      ],
-      "code": "https://github.com/shaman2016scratch/shaman2016scratch.github.io/tree/main/tools"
-    }
-  }
+  ouut = {"ok": true,"result": {"pagesCount": 3,"pages": ["tbc.html","mini-brow.html","search.html"],"code": "https://github.com/shaman2016scratch/shaman2016scratch.github.io/tree/main/tools"}}
   out.innerHTML = ouut
 }
