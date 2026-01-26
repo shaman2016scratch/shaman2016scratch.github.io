5501735df0a3800d1651b2eedb56ae174481c995
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-26 22:41:28 +0300

    Update index.js

--- tools/js/api/index.js
+++ tools/js/api/index.js
@@ -3,5 +3,5 @@ const url = new URL(window.location)
 const input = url.searchParams
 if(!input.get("page") || input.get("page") === "main") {
   ouut = {"ok":true,"result":{"pagesCount":3,"pages":["tbc.html","mini-brow.html","search.html"],"code":"https://github.com/shaman2016scratch/shaman2016scratch.github.io/tree/main/tools"}}
-  out.innerHTML = `<p>JSON.stringify(ouut)</p>`
+  out.innerHTML = `<p>${JSON.stringify(ouut)}</p>`
 }
