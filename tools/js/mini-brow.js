760995e6e901c0449a612ae1f69f5ca0fda2a867
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-26 14:22:57 +0300

    Update mini-brow.js

--- tools/js/mini-brow.js
+++ tools/js/mini-brow.js
@@ -1,4 +1,5 @@
 let screen = document.getElementById("content")
+let latsid = 0
 async function load() {}
 async function save() {}
 async function start() {
@@ -7,7 +8,19 @@ async function start() {
     <button onclick="vkl.add()">add</button>
     <div id="vkladki"></div>
   `
+}
 let vkl = {
-  "add": async function() {},
-  "del": async function() {}
+  "add": async function() {
+    screen.innerHTML += `
+      <div class="messages" id="vkl${lastid}">
+        <button onclick="vkl.del(${lastid})">Delete</button>
+        <button onclick="vkl.set(${lastid}, prompt('url'))">Update Url</button>
+        <iframe src="shaman2016scratch.github.io/tools/search.html"></iframe>
+      </div>
+    `
+    lastid++
+  },
+  "del": async function(id) {},
+  "set": async function(id, url) {}
+
 }
