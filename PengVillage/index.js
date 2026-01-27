4d59c9d203251313ad97a57f4d80e8c272888833
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-27 14:37:39 +0300

    Update index.js

--- PengVillage/index.js
+++ PengVillage/index.js
@@ -2,5 +2,7 @@ let game = document.getElementById('game')
 let menu = document.getElementById('menu')
 let name = document.getElementById('username')
 async function start() {
-  game.innerHTML(`<svg width="800" height="400" xmlns="http://www.w3.org/2000/svg"><text x="0" y="40" font-size="36" fill="black">What is your name? This is necessary for this game.</text></svg>`)
+  game.innerHTML(`
+    <h3>Wecolme to Peng Village Game!</h3>
+  `)
 }
