53b6c61865e005b4f357568ff889caf53f4c140c
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-30 23:27:51 +0300

    Update search.js

--- tools/js/search.js
+++ tools/js/search.js
@@ -7,16 +7,52 @@ const site = {
   "https://shaman2016scratch.github.io": {}
 }
 const page = {
-  "https://yandex.ru/": {},
-  "https://google.com/": {},
-  "https://scratch.mit.edu/": {},
-  "https://github.com/": {},
-  "https://shaman2016scratch.github.io/": {}
+  "https://yandex.ru/": {
+    "searches": ["Yandex", "Яндекс"],
+    "title": "Яндекс — российский поисковик"
+  },
+  "https://google.com/": {
+    "searches": ["Google", "гугл"],
+    "title": "Google"
+  },
+  "https://scratch.mit.edu/": {
+    "searches": ["Scratch", "Скретч", "Скратч"],
+    "title": "Scratch — create, program, explore!"
+  },
+  "https://github.com/": {
+    "searches": ["гитхаб", "github"],
+    "title": "Github is a convenient service for storing code"
+  },
+  "https://shaman2016scratch.github.io/": {
+    "searches": ["shaman2016", "polzovatel_8787"],
+    "title": "Official Site SHAMAN2016"
+  }
 }
 screen.innerHTML = `
   <input placeholder="your request" id="req"></input><button onclick="search()">Search</button>
+  <div id="results">
+    <h2>Results</h2>
+  </div>
 `
 async function save() {}
 async function load() {}
 async function start() {}
-async function search() {}
+async function search() {
+  let zapros = document.getElementById("req").value
+  let results = Object.keys(page)
+  let result = document.getElementById("relusts").innerHTML
+  result = `
+    <h2>Results</h2>
+  `
+  for(let i = 0; i < results.length; i++) {
+    if(page[results[i]].searches.include(zapros)) {
+      result += `
+        <div class="result">
+          <h1>${page[results[i]].title}</h1>
+          <p>${page[results[i]].description || "Без описания"}</p>
+          <a href="${results[i]}">Open</a>
+        </div>
+      `
+    }
+  }
+}
