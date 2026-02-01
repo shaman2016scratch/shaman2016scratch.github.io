c02034576103f62ce84dc596c196a336b897bb97
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-01 23:41:05 +0300

    Update top.js

--- Explore2/top.js
+++ Explore2/top.js
@@ -1,9 +1,7 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./top.css";
-const rootEl = document.getElementById("topboard");
-const root = createRoot(rootEl);
-let projects_req = fetch("https://shaman2016-trampline.vercel.app/scratch/studios/36416387/projects")
-let projects = projects_req.json()
-for(let i = 0; i < projects.length; i++){}
-root.render(app);
+let projects_req
+let projects
+async function projectGet() {
+  projects_req = await fetch("https://shaman2016-trampline.vercel.app/scratch/studios/36416387/projects")
+  projects = await projects_req.json()
+}
+for(let i = 0; i < projects.length; i++) {}
