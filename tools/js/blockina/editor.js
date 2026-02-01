7cd3e35797bb81cd3136c7768393a90fc7054388
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-01 22:43:16 +0300

    Update editor.js

--- tools/js/blockina/editor.js
+++ tools/js/blockina/editor.js
@@ -1,9 +1,15 @@
 let screen = document.getElementById("content")
-async function start() {
-  screen.innerHTML = `
-    <a href="editor.html"><button>EDITOR</button></a>
-    <h1>Blockina</h1>
-    <p>Blockina is a block programming environment inspired by Scratch, TurboWarp and Dash.</p>
-  `
-}
+screen.innerHTML = `
+  <a href="/tools/blockina/"><button>MAIN</button></a>
+  <iframe src="text/html"></iframe>
+  <button onclick="sprite()">Sprites</button><button onclick="code()">Code</button><button onclick="costum()">Costumes</button><button onclick="extLib()">ExtLib</button>
+  <div id="codeCont">
+    <p>Select a section</p>
+  </div>
+`
+async function start() {}
+async function sprite() {}
+async function code() {}
+async function costum() {}
+async function extLib() {}
 window.onload = start
