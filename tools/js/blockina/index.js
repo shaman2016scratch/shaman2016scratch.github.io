9475689a6e439f48fe65e0922784f4c7ad4f92a6
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-01 17:18:14 +0300

    Update index.js

--- tools/js/blockina/index.js
+++ tools/js/blockina/index.js
@@ -1,1 +1,9 @@
-
+let screen = document.getElementById("content")
+async function start() {
+  screen.innerHTML = `
+    <a href="editor.html"><button>EDITOR</button></a>
+    <h1>Blockina</h1>
+    <p>Blockina is a block programming environment inspired by Scratch, TurboWarp and Dash.</p>
+  `
+}
+window.onload = start
