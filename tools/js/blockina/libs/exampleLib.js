18652e841ccd987eb24395be7ebec029aaabd8f5
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-01 23:16:05 +0300

    Create exampleLib.js

---
+++ tools/js/blockina/libs/exampleLib.js
@@ -0,0 +1,20 @@
+let lib = {
+  "name": {
+    "en": "Example Library",
+    "ru": "Пример Библиотеки"
+  },
+  "icon": "https://shaman2016scratch.github.io/tools/blockina/exts/example.svg",
+  "creators": [
+    {
+      "name": "SHAMAN2016",
+      "links": {
+        "github": "https://github.com/shaman2016scratch/",
+        "scratch": "https://scratch.mit.edu/users/shaman2016/"
+      },
+      "docs": "Not"
+      "blocks": [
+        {}
+      ]
+    }
+  ]
+}
