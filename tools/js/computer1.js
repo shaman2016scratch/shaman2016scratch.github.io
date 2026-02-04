8fc964794a1d044abb661ea4b044e70c7c40d01b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-04 21:12:52 +0300

    Update computer1.js

--- tools/js/computer1.js
+++ tools/js/computer1.js
@@ -1,1 +1,12 @@
-
+let screen = document.getElementById("content")
+screen.innerHTML = `
+  <div class="message" id="screen">
+    Start the Computer
+  </div>
+  <br><button>Esc</button><button>F1</button><button>F2</button><button>F3</button><button>F4</button><button>F5</button><button>F6</button><button>F7</button><button>F8</button><button>F9</button><button>F10</button><button>PrtSc</button><button>F11</button><button>F12</button><button>Del</button><button>Sys</button>
+  <br><button>Ё</button><button>OK</button><button>1</button><button>2</button><button>3</button><button>4</button><button>5</button><button>6</button><button>7</button><button>8</button><button>9</button><button>0</button><button>-</button><button>+</button><button>=</button><button>—</button><button>{</button><button>}</button>
+  <br><button>~</button><button>Tab</button><button>Q</button><button>W</button><button>E</button><button>R</button><button>T</button><button>Y</button><button>U</button><button>I</button><button>O</button><button>P</button><button>[</button><button>]</button><button>|</button><button>\</button><button>/</button><button>&</button>
+  <br><button>\`</button><button>Caps</button><button>A</button><button>S</button><button>D</button><button>F</button><button>G</button><button>X</button><button>J</button><button>K</button><button>L</button><button>:</button><button>;</button><button>"</button><button>'</button><button>Enter</button><button>^</button><button>@</button>
+  <br><button>Shift</button><button>Ft</button><button>Clone</button><button>X</button><button>Z</button><button>C</button><button>V</button><button>B</button><button>N</button><button>M</button><button>,</button><button>.</button><button><</button><button>></button><button>?</button><button>!</button><button>#</button><button>*</button>
+  <br><button>(</button><button>)</button><button>%</button><button>$</button><button>₽</button><button>⬅️</button><button>↖️</button><button>⬆️</button><button>↗️</button><button>➡️</button><button>↘️</button><button>⬇️</button><button>↙️</button><button>Ctrl</button><button>Fn</button><button>Win</button><button>Alt</button>
+`
