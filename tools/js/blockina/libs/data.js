49d4441bde9f30a3aae7e49d791f4a67cf1b322e
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-07 00:37:23 +0300

    Update data.js

--- tools/js/blockina/libs/data.js
+++ tools/js/blockina/libs/data.js
@@ -1,1 +1,48 @@
-
+let lib = {
+  "name": {
+    "en": "Data",
+    "ru": "Данные"
+  },
+  "icon": "https://shaman2016scratch.github.io/tools/blockina/exts/data.svg",
+  "creators": [
+    {
+      "name": "SHAMAN2016",
+      "links": {
+        "github": "https://github.com/shaman2016scratch/",
+        "scratch": "https://scratch.mit.edu/users/shaman2016/"
+      },
+      "docs": "Not"
+      "blocks": [
+        {
+          "type": blockina.blockType.LABEL,
+          "text": [],
+          "elements": {
+            "text": "Variables"
+          }
+        },
+        {
+          "type": blockina.blockType
+          "text": [],
+          "elements": {
+            "text": "JSON Object"
+          }
+        },
+        {
+          "type": blockina.blockType.OBJECT,
+          "text": ["text"],
+          "elements": {
+            "text": { "type": "txt", "text": "new object" }
+          },
+          "func": function(args) { return {} }
+        },
+        {
+          "type": blockina.blockType.LABEL,
+          "text": [],
+          "elements": {
+            "text": "JSON Array"
+          }
+        }
+      ]
+    }
+  ]
+}
