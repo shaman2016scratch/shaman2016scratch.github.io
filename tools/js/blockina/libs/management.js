55aa13dc0740e5e1fd9c25efd3084e4983b610bd
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-07 13:18:24 +0300

    Update management.js

--- tools/js/blockina/libs/management.js
+++ tools/js/blockina/libs/management.js
@@ -1,1 +1,28 @@
-
+let lib = {
+  "name": {
+    "en": "Management",
+    "ru": "Управление"
+  },
+  "icon": "https://shaman2016scratch.github.io/tools/blockina/exts/manage.svg",
+  "creators": [
+    {
+      "name": "SHAMAN2016",
+      "links": {
+        "github": "https://github.com/shaman2016scratch/",
+        "scratch": "https://scratch.mit.edu/users/shaman2016/"
+      }
+    }
+  ],
+  "docs": "Not",
+  "blocks": [
+    {
+      "type": blockina.blockType.COMMAND,
+      "text": ["text", "input"],
+      "elements": {
+        "text": { "type": "txt", "text": "wait" },
+        "input": { "type": "inp", "text": "" }
+      },
+      "func": async function(args) { await setTimeout(() => {}, args.input.text*1000) }
+    }
+  ]
+}
