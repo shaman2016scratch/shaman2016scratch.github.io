b5aae7d5cf32feddd7314db1a2c4b359b00ead55
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-07 22:53:42 +0300

    Update terminal.js

--- tools/js/blockina/libs/terminal.js
+++ tools/js/blockina/libs/terminal.js
@@ -1,1 +1,28 @@
-
+let lib = {
+  "name": {
+    "en": "Example Library",
+    "ru": "Пример Библиотеки"
+  },
+  "icon": "https://shaman2016scratch.github.io/tools/blockina/exts/example.svg",
+  "creators": [
+    {
+      "name": "SHAMAN2016",
+      "links": {
+        "github": "https://github.com/shaman2016scratch/",
+        "scratch": "https://scratch.mit.edu/users/shaman2016/"
+      }
+    }
+  ],
+  "docs": "Not",
+  "blocks": [
+    {
+      "type": blockina.blockType.COMMAND,
+      "text": ["text", "input"],
+      "elements": {
+        "text": { "type": "txt", "text": "command" },
+        "input": { "type": "inp", "text": "text, text, texter" }
+      },
+      "func": function(args) { console.log(`hi, text: ${args.input.text}`) }
+    }
+  ]
+}
