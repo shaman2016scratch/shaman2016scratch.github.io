ec01a6894205f1b628957da21d5dc6c380fea06e
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-07 16:28:09 +0300

    Update operators.js

--- tools/js/blockina/libs/operators.js
+++ tools/js/blockina/libs/operators.js
@@ -1,1 +1,74 @@
-
+let lib = {
+  "name": {
+    "en": "Operators",
+    "ru": "Операторы"
+  },
+  "icon": "https://shaman2016scratch.github.io/tools/blockina/exts/operators.svg",
+  "creators": [
+    {
+      "name": "SHAMAN2016",
+      "links": {
+        "github": "https://github.com/shaman2016scratch/",
+        "scratch": "https://scratch.mit.edu/users/shaman2016/"
+      }
+    }
+  ],
+  "docs": "Not",
+  "blocks": [
+    {
+      "type": blockina.blockType.REPORTER,
+      "text": [1, "text", 2],
+      "elements": {
+        "text": { "type": "txt", "text": "+" },
+        1: { "type": "inp", "text": 5 },
+        2: { "type": "inp", "text": 5 }
+      },
+      "func": function(args) { return args[1]text + args[2]text }
+    }, {
+      "type": blockina.blockType.REPORTER,
+      "text": [1, "text", 2],
+      "elements": {
+        "text": { "type": "txt", "text": "-" },
+        1: { "type": "inp", "text": 5 },
+        2: { "type": "inp", "text": 5 }
+      },
+      "func": function(args) { return args[1]text - args[2]text }
+    }, {
+      "type": blockina.blockType.REPORTER,
+      "text": [1, "text", 2],
+      "elements": {
+        "text": { "type": "txt", "text": "*" },
+        1: { "type": "inp", "text": 5 },
+        2: { "type": "inp", "text": 5 }
+      },
+      "func": function(args) { return args[1]text * args[2]text }
+    }, {
+      "type": blockina.blockType.REPORTER,
+      "text": [1, "text", 2],
+      "elements": {
+        "text": { "type": "txt", "text": "÷" },
+        1: { "type": "inp", "text": 5 },
+        2: { "type": "inp", "text": 5 }
+      },
+      "func": function(args) { return args[1]text / args[2]text }
+    }, {
+      "type": blockina.blockType.REPORTER,
+      "text": [1, "text", 2],
+      "elements": {
+        "text": { "type": "txt", "text": "mod" },
+        1: { "type": "inp", "text": 5 },
+        2: { "type": "inp", "text": 5 }
+      },
+      "func": function(args) { return args[1]text % args[2]text }
+    }, {
+      "type": blockina.blockType.REPORTER,
+      "text": [1, "text", 2],
+      "elements": {
+        "text": { "type": "txt", "text": "^" },
+        1: { "type": "inp", "text": 5 },
+        2: { "type": "inp", "text": 5 }
+      },
+      "func": function(args) { return args[1]text ** args[2]text }
+    }
+  ]
+}
