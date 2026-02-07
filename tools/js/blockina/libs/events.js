755f13f518942ef0e84ddc6391058f4b40561a26
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-07 13:07:10 +0300

    Update events.js

--- tools/js/blockina/libs/events.js
+++ tools/js/blockina/libs/events.js
@@ -1,1 +1,27 @@
-
+let lib = {
+  "name": {
+    "en": "Events",
+    "ru": "События"
+  },
+  "icon": "https://shaman2016scratch.github.io/tools/blockina/exts/events.svg",
+  "creators": [
+    {
+      "name": "SHAMAN2016",
+      "links": {
+        "github": "https://github.com/shaman2016scratch/",
+        "scratch": "https://scratch.mit.edu/users/shaman2016/"
+      }
+    }
+  ],
+  "docs": "Not",
+  "blocks": [
+    {
+      "type": blockina.blockType.HAT,
+      "text": ["text"],
+      "elements": {
+        "text": { "type": "txt", "text": "When the project is launched" }
+      },
+      "func": function(args) { return events.sys.projStart }
+    }
+  ]
+}
