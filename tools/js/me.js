3db175370b4fe8826dbaae80405a032203e25831
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-09 20:43:56 +0300

    Update me.js

--- tools/js/me.js
+++ tools/js/me.js
@@ -1,1 +1,18 @@
-
+let screen = document.getElementById("content")
+let result;
+async function getInfo() {
+  result = await fetch("https://api-shaman2016.vercel.app/getMe")
+  result = await result.json()
+  result = result.result
+}
+async function updateInfo() {
+  await getInfo()
+  screen.innerHTML = `
+    <h1>Ip: ${result.ip}</h1>
+    <h2>User Agent: ${result["user-agent"]}</h2>
+    <h3>Language: ${result.headers["x-vercel-ip-country"]}</h3>
+    <h3>City: ${result.headers["x-vercel-ip-city"]}</h3>
+    <h3>Country: ${result.headers["x-vercel-ip-country"]}</h3>
+    <h3>Real IP: ${result.headers["x-real-ip"]}</h3>
+  `
+}
