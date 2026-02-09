911799bdeab29270cbe2d784e495b90cb6b1c54b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-10 00:32:57 +0300

    Update code.js

--- ext-docs/reqExt/code.js
+++ ext-docs/reqExt/code.js
@@ -1,1 +1,40 @@
+// Name: Request Information
+// ID: reqInfoBy8787
+// Description: An extension that receives user data and their device
+// By: SHAMAN2016 <https://scratch.mit.edu/users/SHAMAN2016/>
+// License: MPL-2.0
 
+(function (Scratch) {
+    "use strict";
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error("This Extension must run unsandboxed");
+  }
+  async function getReq() {
+    let a = await fetch("https://api-shaman2016.vercel.app/req")
+    a = await a.json()
+    return a
+  }
+
+    class reqInfoBy8787 {
+      getInfo() {
+        return {
+          id: "reqInfoBy8787",
+          name: "Request Information",
+          color1: "#a3c0e1",
+          blocks: [
+            {
+              opcode: "getUA",
+              blockType: Scratch.BlockType.REPORTER,
+              text: "get User Agent",
+              arguments: {}
+            }
+          ],
+        };
+      }
+async getUA(args) {
+return a.result["user-agent"]
+}
+    }
+    Scratch.extensions.register(new reqInfoBy8787());
+  })(Scratch);
