021427ea35e85222feee0411781a335979cffd54
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-10 12:42:02 +0300

    Update me.js

--- tools/js/me.js
+++ tools/js/me.js
@@ -10,14 +10,15 @@ async function updateInfo() {
    screen.innerHTML = `
     <h1>Ip: ${result.ip}</h1>
     <h2>User Agent: ${result["user-agent"]}</h2>
-    <h3>Language: ${result.headers["accept-language"]}</h3>
-    <h3>City: ${result.headers["x-vercel-ip-city"]}</h3>
-    <h3>Country: ${result.headers["x-vercel-ip-country"]}</h3>
-    <h3>Real IP: ${result.headers["x-real-ip"]}</h3>
-    <h3>Platform: ${result.headers["sec-ch-ua-platform"]}</h3>
-    <h3>Timezone: ${result.headers["x-vercel-ip-timezone"]}</h3>
-    <h3>Origin: ${result.headers["origin"]}</h3>
-    <h3>Continent: ${result.headers["x-vercel-ip-continent"]}</h3>
+    <p>Session Id: ${result.headers["x-invocation-id"]}</p>
+    <p>Language: ${result.headers["accept-language"]}</p>
+    <p>City: ${result.headers["x-vercel-ip-city"]}</p>
+    <p>Country: ${result.headers["x-vercel-ip-country"]}</p>
+    <p>Real IP: ${result.headers["x-real-ip"]}</p>
+    <p>Platform: ${result.headers["sec-ch-ua-platform"]}</p>
+    <p>Timezone: ${result.headers["x-vercel-ip-timezone"]}</p>
+    <p>Origin: ${result.headers["origin"]}</p>
+    <p>Continent: ${result.headers["x-vercel-ip-continent"]}</p>
   `
   let os = ""
   let i = result["user-agent"].split("(")[1]
@@ -38,18 +39,8 @@ async function updateInfo() {
   } else {
     os = i[0]
   }
-  screen.innerHTML = `
-    <h1>Ip: ${result.ip}</h1>
-    <h2>User Agent: ${result["user-agent"]}</h2>
-    <h3>Language: ${result.headers["x-vercel-ip-country"]}</h3>
-    <h3>City: ${result.headers["x-vercel-ip-city"]}</h3>
-    <h3>Country: ${result.headers["x-vercel-ip-country"]}</h3>
-    <h3>Real IP: ${result.headers["x-real-ip"]}</h3>
-    <h3>Platform: ${result.headers["sec-ch-ua-platform"]}</h3>
-    <h3>Timezone: ${result.headers["x-vercel-ip-timezone"]}</h3>
-    <h3>Origin: ${result.headers["origin"]}</h3>
-    <h3>Continent: ${result.headers["x-vercel-ip-continent"]}</h3>
-    <h3>OS: ${os}</h3>
+  screen.innerHTML += `
+    <p>OS: ${os}</p>
   `
 }
 updateInfo()
