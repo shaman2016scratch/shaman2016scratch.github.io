90ed9cb79e3437a23505a2750a1506da91768d64
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-13 00:07:35 +0300

    Update miniwebvm1.js

--- tools/js/miniwebvm1.js
+++ tools/js/miniwebvm1.js
@@ -1,4 +1,16 @@
 let screen = document.getElementById("content")
+let progLang = {
+  "list": [
+    "binMWVM1"
+  ],
+  "info": {
+    "binMWVM1": {}
+  }
+}
 async function start() {}
 async function save() {}
 async function load() {}
+async function runLang1(c) {
+  // binMWVM1
+  let code = c.split(";")
+}
