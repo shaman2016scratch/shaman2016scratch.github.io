9d58c09eb03fd802f7163abd21d158fcaae7b0c6
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-15 23:27:47 +0300

    Update promopv.js

--- promopv.js
+++ promopv.js
@@ -14,4 +14,21 @@ if (par.get() === "info") {
   body.innerHTML = `
     <h1>Promo Preview</h1>
   `
-} else if (par.get() === "AAA") {}
+} else if (par.get() === "AAA") {
+  body.innerHTML = `
+    <h1>MiCloud</h1>
+    <p>MiCloud — Новый сервис для создания файлов</p>
+    <h2>На нём можно:</h2>
+    <ul>
+      <li>Создавать и хранить файлы</li>
+      <li>Хостить сайты¹</li>
+      <li>Создавать репозитории</li>
+      <li>Делится файлами</li>
+    </ul>
+    <p>Также присутствуют следущие удобности:</p>
+    <ul>
+      <li>Web-версия client.micloud.website</li>
+    </ul>
+    <p>¹ — бесплатно на micloud.website/userId/repoId, платно имяРепозитория.micloud.website</p>
+  `
+}
