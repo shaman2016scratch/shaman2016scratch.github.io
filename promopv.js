1b61322f3293f05574962ae9300bde51e101332d
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-15 22:23:02 +0300

    Update promopv.js

--- promopv.js
+++ promopv.js
@@ -1,2 +1,13 @@
+const url = new URL(window.location)
+const sP = url.searchParams
 let head = document.head
 let body = document.body
+let par = {
+  "get": function() { return sP.get("id") },
+  "set": function(a) { sP.set("id", a) },
+  "append": function(a) { sP.append("id", a) }
+}
+if (!par.get()) {
+  par.append("info")
+}
+if (par.get() === "info") {}
