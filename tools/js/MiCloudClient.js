848550fd35fa8d3824438e8a1a021275c2520778
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-22 14:30:34 +0300

    Update MiCloudClient.js

--- tools/js/MiCloudClient.js
+++ tools/js/MiCloudClient.js
@@ -2,4 +2,10 @@ let baseApi = "https://api.micloud.website/"
 let httpApi = baseApi
 let screen = document.getElementById("content")
 let token = document.getElementById("clientKey")
-async function start() {}
+async function start() {
+  screen.innerHTML = `
+    <p>Please select the MiCloud part</p>
+    <br><button>Files</button>
+    <br><button>Repositories</button>
+  `
+}
