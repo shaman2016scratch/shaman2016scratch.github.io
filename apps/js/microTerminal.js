bc907e9ff5e5469c58313da93641581541603405
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-22 10:04:07 +0300

    Update microTerminal.js

--- apps/js/microTerminal.js
+++ apps/js/microTerminal.js
@@ -1,1 +1,12 @@
-
+let screen = document.getElementById("content")
+const package = {
+  "name": "Micro Terminal",
+  "version": "0.0.1",
+  "creator": "SHAMAN2016",
+  "type": "console",
+  "versions": ["0.0.1"],
+  "depends": {}
+}
+let dataStorage = {
+  "var": {}
+}
