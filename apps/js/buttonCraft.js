c43518ad64ac4c8c5f476a3069f30f3c9e34034a
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-22 10:02:49 +0300

    Update buttonCraft.js

--- apps/js/buttonCraft.js
+++ apps/js/buttonCraft.js
@@ -1,1 +1,12 @@
-
+let screen = document.getElementById("content")
+const package = {
+  "name": "buttonCraft",
+  "version": "0.0.1",
+  "creator": "SHAMAN2016",
+  "type": "game",
+  "versions": ["0.0.1"],
+  "depends": {}
+}
+let dataStorage = {
+  "var": {}
+}
