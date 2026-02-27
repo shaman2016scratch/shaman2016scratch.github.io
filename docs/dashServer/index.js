b97dab739ffb18008df99015715ace69d65fc2b6
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-28 00:34:52 +0300

    Update index.js

--- docs/dashServer/index.js
+++ docs/dashServer/index.js
@@ -6,21 +6,18 @@ let langs = [
 let pages = {
   "main": {
     "contentRu": `
-      <h1>Other Pages in documentation</h1>
-      <p><button>Main</button></p>
+      <h1>Оглавление</h1>
       <p><button>Cors</button></p>
-      <h1>Dash Server Document (Unofficial)</h1>
+      <h1>Документация по серверу Dash (Неофициально)</h1>
       <p>Сервер Dash появился в конце февраля 2026 года</p>
       <p>Он захосчен на Vercel и хранит данные в Telegram</p>
     `,
     "contentEn": `
-      <h1>Other Pages in documentation</h1>
-      <p><button>Main</button></p>
+      <h1>Table of contents</h1>
       <p><button>Cors</button></p>
       <h1>Dash Server Documentation (Unofficial)</h1>
       <p>The Dash server appeared at the end of February 2026</p>
       <p>It is hosted on Vercel and stores data in Telegram</p>
     `
-  },
-  "cors": {}
+  }
 }
