589a4c0d10867009faa8eddb02e3d04c3012849d
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-28 00:37:43 +0300

    Update index.js

--- docs/dashServer/index.js
+++ docs/dashServer/index.js
@@ -3,10 +3,15 @@ let langs = [
   "RU",
   "EN"
 ]
+let data = {
+  "lastUpd": "28.02.2026 00:37 UTC+3",
+  "created": "28.02.2026 00:19 UTC+3"
+}
 let pages = {
   "main": {
     "contentRu": `
       <h1>Оглавление</h1>
+      <p><button>Методы</button></p>
       <p><button>Cors</button></p>
       <h1>Документация по серверу Dash (Неофициально)</h1>
       <p>Сервер Dash появился в конце февраля 2026 года</p>
@@ -14,6 +19,7 @@ let pages = {
     `,
     "contentEn": `
       <h1>Table of contents</h1>
+      <p><button>Methods</button></p>
       <p><button>Cors</button></p>
       <h1>Dash Server Documentation (Unofficial)</h1>
       <p>The Dash server appeared at the end of February 2026</p>
