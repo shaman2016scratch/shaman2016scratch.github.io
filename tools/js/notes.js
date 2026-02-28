8606175a2310f311caea5fcb3dfdf479cc611c66
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-02-28 14:53:02 +0300

    Update notes.js

--- tools/js/notes.js
+++ tools/js/notes.js
@@ -1,4 +1,18 @@
 let screen = document.getElementById("content")
-async function start() {}
+screen.innerHTML = `
+  <h1>Notes Storage</h1>
+  <p>Text</p>
+  <p>Id</p>
+  <div id="notes" id="message">
+    <p id="main">Hello, world</p>
+  </div>
+`
+let notes = {
+  "main": {
+    "content": document.getElementById("main")
+  }
+}
+notes.main.content.textContent = "Hello, world!"
+let noteList = document.getElementById("notes")
 async function save() {}
 async function load() {}
