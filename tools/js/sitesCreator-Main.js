c340c4f7f61dc37b870a8645ee78d7ba409b3f51
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-15 23:07:27 +0300

    Update sitesCreator-Main.js

--- tools/js/sitesCreator-Main.js
+++ tools/js/sitesCreator-Main.js
@@ -1,3 +1,16 @@
-let screen = document.getElementById("content")
-window.sitesCreatorsMain = {}
-async function start() {}
+window.screen = document.getElementById("content")
+window.sitesCreatorsMain = {
+  "openEditor": async function() {},
+  "openFilesList": async function() {},
+  "window": {}
+}
+async function start() {
+  window.screen.innerHTML = `
+    <button onclick="window.sitesCreatorsMain.openEditor()">Creator</button>
+    <button onclick="window.sitesCreatorsMain.openFilesList()">Files</button>
+    <div id="menu">
+      <p>Choose.</p>
+    </div>
+  `
+  window.creatorDOM = document.getElementById("menu")
+}
