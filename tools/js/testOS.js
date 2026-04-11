3d92648791143dcb352cf4951de842c8e3c91970
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-11 12:02:23 +0300

    Update testOS.js

--- tools/js/testOS.js
+++ tools/js/testOS.js
@@ -7,7 +7,8 @@ let data = {
   ],
   userData: {
     'test user': {
-      name: 'test'
+      name: 'test',
+      password: null
     }
   },
   mainData: {
@@ -29,15 +30,37 @@ let data = {
     components: {
       list: [],
       metadata: {}
-    },
-    cloud: {
-      1: {},
-      2: {},
-      3: {}
     }
+  },
+  cloud: {
+    1: {},
+    2: {},
+    3: {}
   }
 }
-async function start() {}
+async function start() {
+  try {
+    root.innerHTML = `
+      <h1>Please, wait. System loading</h1>
+    `
+    root.innerHTML = `
+      <h1>System starting</h1>
+    `
+    root.innerHTML = `
+      <h1>System Started!</h1>
+    `
+  } catch (error) {
+    console.error('ERROR DATA')
+    console.error(error)
+    console.log('Please report of error')
+    root.innerHTML = `
+      <h1>ERROR</h1>
+      <p>${error.message}</p>
+      <p>Open browser console for information of error</p>
+      <p>Please report of error <a href='https://scratch.mit.edu/users/SHAMAN2016'>in scratch</a> <a href='https://github.com/shaman2016scratch/shaman2016scratch.github.io/'>or github</a></p>
+    `
+  }
+}
 document.addEventListener('DOMContentLoaded', (e) => {
   start()
 })
