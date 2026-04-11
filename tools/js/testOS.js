7c33476dbd0735e435d73b12e78ff786345afe84
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-11 11:47:56 +0300

    Create testOS.js

---
+++ tools/js/testOS.js
@@ -0,0 +1,43 @@
+let root = document.body
+let data = {
+  apps: [],
+  appData: {},
+  users: [
+    'test user'
+  ],
+  userData: {
+    'test user': {
+      name: 'test'
+    }
+  },
+  mainData: {
+    databases: {
+      metadata: {
+        main: {
+          tables: {}
+        }
+      }
+    }
+  },
+  filesystem: {},
+  system: {
+    metadata: {
+      name: 'Test8787OS',
+      version: '0.0.1 alpha',
+      buildId: 1
+    },
+    components: {
+      list: [],
+      metadata: {}
+    },
+    cloud: {
+      1: {},
+      2: {},
+      3: {}
+    }
+  }
+}
+async function start() {}
+document.addEventListener('DOMContentLoaded', (e) => {
+  start()
+})
