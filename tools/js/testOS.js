4389a7fbfd3c02e8842e58533a504ece947c6a91
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-11 12:31:39 +0300

    Update testOS.js

--- tools/js/testOS.js
+++ tools/js/testOS.js
@@ -38,16 +38,26 @@ let data = {
     3: {}
   }
 }
+let user = null
 async function start() {
   try {
+    user = null
     root.innerHTML = `
       <h1>Please, wait. System loading</h1>
     `
     root.innerHTML = `
-      <h1>System starting</h1>
+      <h1>Please, wait. System starting</h1>
     `
+    let accounts = `<h2>Accounts</h2>`
+    for(let i = 0; i < data.users.length; i++) {
+      let userLoginInfo = data.userData[data.users[i]]
+      accounts += `
+        <p onclick='login(${data.users[i]})'>${userLoginInfo.name}</p>
+      `
+    }
     root.innerHTML = `
       <h1>System Started!</h1>
+      <div id='login'>${accounts}</div>
     `
   } catch (error) {
     console.error('ERROR DATA')
@@ -57,7 +67,34 @@ async function start() {
       <h1>ERROR</h1>
       <p>${error.message}</p>
       <p>Open browser console for information of error</p>
-      <p>Please report of error <a href='https://scratch.mit.edu/users/SHAMAN2016'>in scratch</a> <a href='https://github.com/shaman2016scratch/shaman2016scratch.github.io/'>or github</a></p>
+      <p>Please report of error <a href='https://scratch.mit.edu/users/SHAMAN2016'>in scratch</a> <a href='https://github.com/shaman2016scratch/shaman2016scratch.github.io/issues'>or github</a></p>
+    `
+  }
+}
+function login(userId) {
+  let userLoginInfo = data.userData[userId]
+  if(userLoginInfo.password === null) {
+    user = userId
+    openDesktop()
+  } else {
+    let loginMenu = document.getElementById('login')
+    loginMenu.innerHTML = `
+      <h3>Login</h3>
+      <label for='pass'>Password</label>
+      <input id='pass'>
+      <button onclick='loginAcc(${userId}, document.getElementById("pass").value)'>LOGIN</button>
+    `
+  }
+}
+function loginAcc(userId, pass) {
+  let userLoginInfo = data.userData[userId]
+  if(userLoginInfo.password === pass) {
+    user = userId
+    openDesktop()
+  } else {
+    let loginMenu = document.getElementById('login')
+    loginMenu.innerHTML += `
+      <p>The password is incorrect</p>
     `
   }
 }
