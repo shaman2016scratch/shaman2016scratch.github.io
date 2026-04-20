fac605272a6fd6e8cee46bbba7e1308d2b433326
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-20 16:26:39 +0300

    Update code.js

--- ext-docs/altLists/code.js
+++ ext-docs/altLists/code.js
@@ -1,1 +1,190 @@
+// Name: Alternative Lists
+// ID: shaman2016list
+// Description: Alternative lists
+// By: SHAMAN2016 <https://scratch.mit.edu/users/SHAMAN2016/>
+// License: MPL-2.0
 
+(function (Scratch) {
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error("This Extension must run unsandboxed");
+  }
+
+    class shaman2016list {
+      constructor() {
+        this.lists = {}
+      }
+      
+      getInfo() {
+        return {
+          id: "shaman2016list",
+          name: 'lists',
+          docsURL: "https://shaman2016scratch.github.io/ext-docs/altLists/",
+          color1: "#ff4a00",
+          blocks: [
+            {
+              opcode: "getList",
+              blockType: Scratch.BlockType.ARRAY,
+              text: "get array from list [list]",
+              arguments: {
+                 list: {
+                  defaultValue: "my list",
+                  type: Scratch.ArgumentType.STRING,
+                }
+              }
+            }, {
+              opcode: "setItemList",
+              blockType: Scratch.BlockType.COMMAND,
+              text: "set item [item] value [value] in list [list]",
+              arguments: {
+                 list: {
+                  defaultValue: "my list",
+                  type: Scratch.ArgumentType.STRING,
+                },
+                item: {
+                  defaultValue: 1,
+                  type: Scratch.ArgumentType.NUMBER,
+                },
+                value: {
+                  defaultValue: "item",
+                  type: Scratch.ArgumentType.STRING,
+                }
+              }
+            }, {
+              opcode: "newItemList",
+              blockType: Scratch.BlockType.COMMAND,
+              text: "new item [value] in list [list]",
+              arguments: {
+                 list: {
+                  defaultValue: "my list",
+                  type: Scratch.ArgumentType.STRING,
+                },
+                value: {
+                  defaultValue: "item",
+                  type: Scratch.ArgumentType.STRING,
+                }
+              }
+            }, {
+              opcode: "getItemList",
+              blockType: Scratch.BlockType.REPORTER,
+              text: "get item [item] in list [list]",
+              arguments: {
+                 list: {
+                  defaultValue: "my list",
+                  type: Scratch.ArgumentType.STRING,
+                },
+                item: {
+                  defaultValue: 1,
+                  type: Scratch.ArgumentType.NUMBER,
+                }
+              }
+            }, {
+              opcode: "getLengthList",
+              blockType: Scratch.BlockType.REPORTER,
+              text: "get length list [list]",
+              arguments: {
+                 list: {
+                  defaultValue: "my list",
+                  type: Scratch.ArgumentType.STRING,
+                }
+              }
+            }, {
+              opcode: "deleteItemList",
+              blockType: Scratch.BlockType.COMMAND,
+              text: "delete item [item] in list [list]",
+              arguments: {
+                 list: {
+                  defaultValue: "my list",
+                  type: Scratch.ArgumentType.STRING,
+                },
+                item: {
+                  defaultValue: 1,
+                  type: Scratch.ArgumentType.NUMBER,
+                }
+              }
+            }, {
+              opcode: "getItemIndexList",
+              blockType: Scratch.BlockType.REPORTER,
+              text: "get item for value [value] in list [list]",
+              arguments: {
+                 list: {
+                  defaultValue: "my list",
+                  type: Scratch.ArgumentType.STRING,
+                },
+                value: {
+                  defaultValue: "item",
+                  type: Scratch.ArgumentType.STRING,
+                }
+              }
+            }, {
+              opcode: "setList",
+              blockType: Scratch.BlockType.COMMAND,
+              text: "set list [list] of array [array]",
+              arguments: {
+                 list: {
+                  defaultValue: "my list",
+                  type: Scratch.ArgumentType.STRING,
+                },
+                array: {
+                  defaultValue: ['one item', 'two item', 'index the item = 2'],
+                  type: Scratch.ArgumentType.ARRAY,
+                }
+              }
+            }, {
+              blockType: Scratch.BlockType.LABEL,
+              text: 'Save'
+            }, {
+              opcode: "importL",
+              blockType: Scratch.BlockType.COMMAND,
+              text: "import [key]",
+              arguments: {
+                 key: {
+                  defaultValue: {},
+                  type: Scratch.ArgumentType.STRING,
+                }
+              }
+            }, {
+              opcode: "exportL",
+              blockType: Scratch.BlockType.REPORTER,
+              text: "export",
+              arguments: {}
+            }
+          ],
+        };
+      }
+      getList(args) {
+        return this.lists[args.list].value
+      }
+      setItemList(args) {
+        this.lists[args.list].value[args.item].text = args.value
+      }
+      newItemList(args) {
+        this.lists[args.list].value.push({
+          text: args.value,
+          created: new Date()
+        })
+      }
+      getItemList(args) {
+        return this.lists[args.list].value[args.item].text
+      }
+      getLengthList(args) {
+        return this.lists[args.list].value.length
+      }
+      deleteItemList(args) {
+        this.lists[args.list].value.splice(args.item, 1)
+      }
+      getItemIndexList(args) {
+        return this.lists[args.list].value.indexOf(args.value)
+      }
+      setList(args) {
+        this.lists[args.list].value = args.array
+      }
+      importL(args) {
+        this.lists = args.key
+      }
+      exportL() {
+        return this.lists
+      }
+    }
+    Scratch.extensions.register(new shaman2016list());
+  })(Scratch);
