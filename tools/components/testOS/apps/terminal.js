3fc442734bfa712f8c2854f33eb43597db32a631
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-22 00:09:26 +0300

    Update terminal.js

--- tools/components/testOS/apps/terminal.js
+++ tools/components/testOS/apps/terminal.js
@@ -3,9 +3,9 @@ if (!window.terminalInstalled) {
   window.terminal.import = function(imdata) {}
   window.testOSterminal = {}
   window.terminal.importDepends = await function() {
-    const installDepend = new Function(['params'], await (await fetch('https://raw.githubusercontent.com/shaman2016scratch/shaman2016scrqtch.github.io/main/tools/components/testOS/terminal/pkg.js')).text())
+    const installDepend = new Function(['params'], `async function asyncInstall() { ${await (await fetch('https://raw.githubusercontent.com/shaman2016scratch/shaman2016scrqtch.github.io/main/tools/components/testOS/terminal/pkg.js')).text()} }; asyncInstall()`)
     window.terminal.depend = {}
-    window.terminal.depend['@terminal/logic'] = installDepend({
+    window.terminal.depend['@terminal/logic'] = await installDepend({
       pkg: '@terminal/logic'
     })
   }
