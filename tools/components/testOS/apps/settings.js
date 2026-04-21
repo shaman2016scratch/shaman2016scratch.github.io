0532435cbb7957b4ea305d7bfea17a39aebae9d7
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-21 18:29:48 +0300

    Update settings.js

--- tools/components/testOS/apps/settings.js
+++ tools/components/testOS/apps/settings.js
@@ -1,1 +1,8 @@
+window.appSdk.console.log('Settings started', 'settings.logger')
 
+window.settingsOS = {}
+
+window.settingsOS.import = async function(package, module) {
+  const pkg = await (await fetch(``)).json()
+}
+window.settingsOS.pkgVersion = 'main'
