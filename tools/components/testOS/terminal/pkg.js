8e817986bcf7a84b50e7b1559d05506c8b73f8fd
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-22 00:08:23 +0300

    Update pkg.js

--- tools/components/testOS/terminal/pkg.js
+++ tools/components/testOS/terminal/pkg.js
@@ -1,17 +1,28 @@
-const installDepend = new Function(['params'], await (await fetch('https://raw.githubusercontent.com/shaman2016scratch/shaman2016scrqtch.github.io/main/tools/components/testOS/terminal/pkg.js')).text())
+const installDepend = new Function(['params'], `async function asyncInstall() { ${await (await fetch('https://raw.githubusercontent.com/shaman2016scratch/shaman2016scrqtch.github.io/main/tools/components/testOS/terminal/pkg.js')).text()} }; await asyncInstall()`)
 let out = ''
 if (params.pkg === '@terminal/logic') {
   let myDepends = {}
-  myDepends['@terminal/parser'] = new Function(['params'], installDepend({
+  myDepends['@terminal/parser'] = new Function(['params'], await installDepend({
     pkg: '@terminal/parser'
   }))
-  myDepends['@terminal/runner'] = new Function(['params'], installDepend({
+  myDepends['@terminal/runner'] = new Function(['params'], await installDepend({
     pkg: '@terminal/runner'
   }))
+  out = {
+    run: async function(code) {
+      return (await myDepends['@terminal/runner'](await myDepends['@terminal/parser']().parse(code)))
+    }
+  }
 } else if (params.pkg === '@terminal/parser') {
   let myDepends = {}
+  out = function() {
+    return {
+      parse: async function(code) {}
+    }
+  }
 } else if (params.pkg === '@terminal/runner') {
   let myDepends = {}
+  out = async function(code) {}
 }
 
 return out
