7bc6a40a86277f8aad4add4f3be7ea239dd6e98a
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-21 23:53:33 +0300

    Update terminal.js

--- tools/components/testOS/apps/terminal.js
+++ tools/components/testOS/apps/terminal.js
@@ -1,1 +1,12 @@
-
+if (!window.terminalInstalled) {
+  window.terminal = {}
+  window.terminal.import = function(imdata) {}
+  window.testOSterminal = {}
+  window.terminal.importDepends = await function() {
+    const installDepend = new Function(['params'], await (await fetch('https://raw.githubusercontent.com/shaman2016scratch/shaman2016scrqtch.github.io/main/tools/components/testOS/terminal/pkg.js')).text())
+    window.terminal.depend = {}
+    window.terminal.depend['@terminal/logic'] = new Function(['params'], installDepend('@terminal/logic'))
+  }
+  window.terminal.importDepends()
+  window.terminalInstalled = true
+}
