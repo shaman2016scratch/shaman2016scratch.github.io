1d6fc02670f3b8f5be84bee044b93c072e29d53c
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-05-18 04:37:20 +0300

    Update getDashUser~urlUpd.js

--- tools/js/getDashUser~urlUpd.js
+++ tools/js/getDashUser~urlUpd.js
@@ -1,6 +1,6 @@
 self.addEventListener('message', (event) => {
   while (true) {
-    if (document.getElementById('projects').src !== `/tools/getDashProjectByUser?id=${event.payload.uid}`) {
+    if (event.payload.projects.src !== `/tools/getDashProjectByUser?id=${event.payload.uid}`) {
       event.payload.location.href = event.payload.projects.src
     }
   }
