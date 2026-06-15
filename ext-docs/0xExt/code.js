b45ea252ac0835f389f342c21fdd916333af14f8
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-06-15 13:03:40 +0300

    Update code.js

--- ext-docs/0xExt/code.js
+++ ext-docs/0xExt/code.js
@@ -1,5 +1,5 @@
-// Name: Hexadecimal numbers
-// ID: hexnumBypolzovatel8787
+// Name: Numbers+
+// ID: numsPlusBypolzovatel8787
 // Description: 
 // By: polzovatel_8787 <https://dashblocks.github.io/#7>
 // License: MPL-2.0
@@ -9,27 +9,44 @@
   if (!Scratch.extensions.unsandboxed) {
     throw new Error("This Extension must run unsandboxed");
   }
-  constructor () {
-    this.hexnums = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
-  }
 
-    class HexnumBypolzovatel8787 {
+    class numsPlusBypolzovatel8787 {
+      constructor () {
+        this.hexnums = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+      }
       getInfo() {
         return {
-          id: "hexnumBypolzovatel8787",
-          name: "Hexadecimal numbers",
+          id: "numsPlusBypolzovatel8787",
+          name: "Numbers+",
           docsURL: "https://shaman2016scratch.github.io/ext-docs/0xExt/",
           color1: "#005c10",
           blocks: [
             {
+              opcode: "n10toOtherNum",
+              blockType: Scratch.BlockType.ARRAY,
+              text: "number [num] using [sym] characters",
+              arguments: {
+                num: {
+                  defaultValue: 255,
+                  type: Scratch.ArgumentType.NUMBER,
+                },
+                sym: {
+                  defaultValue: 2,
+                  type: Scratch.ArgumentType.NUMBER,
+                }
+              }
+            }, {
+              blockType: Scratch.BlockType.LABEL,
+              text: 'hexadecimal numbers'
+            }, {
               opcode: "hexlist",
               blockType: Scratch.BlockType.ARRAY,
-              text: "Hex numbers",
+              text: "hex numbers",
               arguments: {}
             }, {
               opcode: "hexcodeof",
               blockType: Scratch.BlockType.REPORTER,
-              text: "Hex code of [num]",
+              text: "hex code of [num]",
               arguments: {
                 num: {
                   defaultValue: 255,
@@ -47,5 +64,5 @@
         return args.num.toString(16)
       }
     }
-    Scratch.extensions.register(new HexnumBypolzovatel8787());
+    Scratch.extensions.register(new numsPlusBypolzovatel8787());
   })(Scratch);
