134b20ee39ca7e82e862ac7ef496cabf108d92cc
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-06-29 19:14:30 +0300

    Create logic for SOSearch (пока только бд)

---
+++ tools/js/sosirsOSSearch.js
@@ -0,0 +1,65 @@
+const searchData = {
+  sites: {
+    "dashblocks.org": {},
+    "turbowarp.org": {},
+    "scratch.mit.edu": {},
+    "shaman2016scratch.github.io": {}
+  },
+  pages: {
+    "https://dashblocks.org/donate": {
+      title: "Donate - Dash",
+      description: "Донат Dash'у"
+    },
+    "https://dashblocks.org/": {
+      title: "Dash - больше возможностей для редактора",
+      description: "Dash - больше возможностей для редактора"
+    },
+    "https://dashblocks.org/editor": {
+      title: "Dash - больше возможностей для редактора",
+      description: "Dash - больше возможностей для редактора"
+    },
+    "https://dashblocks.org/extensions/": {
+      title: "Dash Extensions Gallery",
+      description: ""
+    },
+    "https://turbowarp.org/": {
+      title: "TurboWarp - Запускайте проекты Scratch быстрее!"
+    },
+    "https://scratch.mit.edu/": {
+      title: "Scratch - Фантазируй, программируй, делись!"
+    },
+    "https://shaman2016scratch.github.io/": {
+      title: "Официальный сайт polzovatel_8787"
+    },
+    "https://shaman2016scratch.github.io/sosirsOS/": {
+      title: "Официальный сайт sosirsOS",
+      note: "Если быть точнее - официальная страница sosirsOS"
+    },
+    "https://shaman2016scratch.github.io/shaman2016.github.io/": {
+      title: "Официальный сайт SHAMAN2016"
+    },
+    "https://shaman2016scratch.github.io/shaman2016.github.io/wiki/": {
+      title: "SHAMAN2016-WIKI",
+      description: "Официальная вики про SHAMAN2016 и его проекты"
+    },
+    "https://shaman2016scratch.github.io/shaman2016.github.io/wiki/sosirsOS/": {
+      title: "sosirsOS | SHAMAN2016-WIKI",
+      description: "Всё про sosirsOS - что это? Кто его создал? Какие у него есть версии и дистрибутивы? Документации по нему, а также подробности!"
+    }
+  },
+  descriptions: {
+    dash: [
+      "Dash - модификация TurboWarp и других модов.<br>Dash имеет комьюнити, включая: публикацию проектов, подписка на аккаунты, вход и регистрация, поджигание проектов и многое другое...<br>Dash создается небольшой командой разработчиков.<br>Также, рекомендуемо <a href='https://dashblocks.org/donate'>задонатить в Dash</a> чтобы в нем всегда было комьюнити.",
+      "Dash - лучшая модификация Скретча.<br><div onclick='window.open('https://dashblocks.org/')'>Попробывать Dash</div>"
+    ],
+    sosirsOS: [
+      "sosirsOS - Операционная система (ОС) на Dash (до 3 июня 2026 года - на Scratch), создана Интерграмом и разрабатывается им.",
+      "sosirsOS - Многофункциональная ОС на Dash, создана одним из младших разработчиков Dash и разрабатывается им.",
+      "<b>Подробно про sosirsOS</b>:<br><ul><li>Создана Интерграмом (а конкретно polzovatel_8787), и разрабатывается им, но вместе с комьюнити и Drix Foundation</li><li>sosirsOS раньше находилась на Scratch (до 3 июня 2026 года) и имела много функционала, 3 июня 2026 года была перенесена на Dash</li><li>sosirsOS вполне нормальная функционалом ОС, скорее всего, все ОС уступают sosirsOS по функционалу</li><li>Хотите узнать ещё - рекомендую прочитать <a href='https://shaman2016scratch.github.io/shaman2016.github.io/wiki/sosirsOS'>статью о sosirsOS которая в основеном редактируется создателем и главным разработчиком sosirsOS - polzovatel_8787</a></li></ul>"
+    ]
+  },
+  tags: []
+}
+let result = document.getElementById("result")
+
+async function search() {}
